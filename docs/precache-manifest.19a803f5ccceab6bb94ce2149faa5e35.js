6e6a8232fb1ed38960b1f0c9b63d3614697e96a0
GurjeevPal <gurjeev.pal@gmail.com>
2020-08-06 16:15:09 -0400

    moving github repo  docs

---
+++ docs/precache-manifest.19a803f5ccceab6bb94ce2149faa5e35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2af3c3ad485984f73061ab97d9ccaa8",
+    "url": "/gurjeevpal/Portfolio/index.html"
+  },
+  {
+    "revision": "dfcde9d460a99625ed8c",
+    "url": "/gurjeevpal/Portfolio/static/css/main.a80ac5c0.chunk.css"
+  },
+  {
+    "revision": "a5183a887152925b4b8d",
+    "url": "/gurjeevpal/Portfolio/static/js/2.ffbb7f8e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gurjeevpal/Portfolio/static/js/2.ffbb7f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcde9d460a99625ed8c",
+    "url": "/gurjeevpal/Portfolio/static/js/main.34c79183.chunk.js"
+  },
+  {
+    "revision": "720bdbc6c7c93f24bb07",
+    "url": "/gurjeevpal/Portfolio/static/js/runtime-main.5847bcde.js"
+  },
+  {
+    "revision": "9462c4cc555ffb147ed1e013935db27c",
+    "url": "/gurjeevpal/Portfolio/static/media/Gurjeev.9462c4cc.png"
+  }
+]);
